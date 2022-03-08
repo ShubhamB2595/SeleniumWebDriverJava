820832ced02751d1e50536c38535ba4f2a9e3a2e
ShubhamB2595 <shubhambhele95@gmail.com>
2022-03-08 18:08:30 +0530

    Code ADDED for WebDriverManager

--- src/main/java/WDManager.java
+++ src/main/java/WDManager.java
@@ -1,23 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SWebDriver {
+public class WDManager {
 
     public static String WEB_URL = "https://www.ebay.com/";
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "I:\\Installation Files\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-
+        
         try {
             driver.manage().window().maximize();
             driver.get(WEB_URL);
             Thread.sleep(4000);
             driver.close();
-
         } catch (Exception e) {
             System.out.println("Exception: " + e.getMessage());
         }
+        
     }
 }
